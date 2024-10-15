dcd9908c294d21a24265f881526172f94d8ab3ce
Lucas Feliciano <lucas.for.study.42@gmail.com>
2024-10-15 11:22:43 -0300

    Building 2/2

--- PyCriptoNita.py
+++ PyCriptoNita.py
@@ -1,6 +1,6 @@
 import string
 import random
-
+print('.PyCriptoNita')
 
 def show_banner():
     banner = """
@@ -14,29 +14,57 @@ def show_banner():
     """
     print(banner)
 
-all_characters = string.ascii_uppercase + string.ascii_lowercase + string.digits + " ÇçÃãÕõÁáÉéÍíÓóÚúÂâÊêÎîÔôÛûÀàÈèÌìÒòÙùÄäËëÏïÖöÜü"
-print(all_characters)
+
+# Todos os caracteres, incluindo letras, dígitos, pontuações e caracteres especiais
+all_characters = (
+    string.ascii_uppercase +  # Letras maiúsculas A-Z
+    string.ascii_lowercase +  # Letras minúsculas a-z
+    string.digits +           # Dígitos 0-9
+    string.punctuation +      # Pontuações
+    " ñÑÇçÃãÕõÁáÉéÍíÓóÚúÂâÊêÎîÔôÛûÀàÈèÌìÒòÙùÄäËëÏïÖöÜü"  # Caracteres especiais
+)
+
+#print(all_characters)
+
 def key_generator(all_characters):
     list_characters = list(all_characters)
     random.shuffle(list_characters)
     key = ''.join(list_characters)
     return key
-
+key = key_generator(all_characters)
 def input_msg():
+
     while True:
         msg = input("Enter your message: ")
         if len(msg) < 1:
-            print('\n- insert a larger message.\n')
+            print('\n- Insert a larger message.\n')
         elif msg.isspace():
-            print('\n- it is just space.\n')
+            print('\n- It is just space.\n')
         else:
             return msg
-            break
 
 def encrypt_msg(msg):
-    encrypted = ''
+    criptonita = ''
     for i in range(len(msg)):
-        if msg[i] in all_characters:
+        if msg[i] in key:
+            x = key.index(msg[i])
+            criptonita += key[(x + 1) % len(key)]
+        else:
+            criptonita += msg[i]
+    return criptonita
 
-msg = input_msg()
-print(msg)
+def decrypt_msg(encrypted_msg):
+    original_msg = ''
+    for i in range(len(encrypted_msg)):
+        if encrypted_msg[i] in key:
+            x = key.index(encrypted_msg[i])
+            original_msg += key[(x - 1) % len(key)]
+        else:
+            original_msg += encrypted_msg[i]
+    return original_msg
+
+# Testando
+'''
+decrypted_message = decrypt_msg(encrypted_message)
+print("Decrypted Message:", decrypted_message)
+'''
--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 from PyCriptoNita import *
 show_banner()
+
+message = input_msg()
+encrypted_message = encrypt_msg(message)
+print("Encrypted Message:", encrypted_message)
