5b2723bf4c381961f76370d341f1a75e5f970bb1
NASCC <pedro.main.tec@gmail.com>
2024-10-20 21:26:34 -0300

    [ADDED] main.py can now be called via shell script

---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# armazenando nossas colorescapes
+RESET_ESCAPE='\e[0m'
+ERROR_ESCAPE='\e[1;31m'
+SUCES_ESCAPE='\e[1;32m'
+
+# caminho do program principal
+PROGRAM_PATH=./src/main.py
+
+# se o arquivo citado anteriormente existe, printar sucesso e chamar arquivo
+if [ -f "$PROGRAM_PATH" ]; then
+  echo ;
+  echo -e "Program file$SUCES_ESCAPE was found!$RESET_ESCAPE";
+  echo -n "It will run in ";
+  for i in {3..1}; do
+    echo -n "$i ";
+    sleep 1;
+  done ;
+  echo ;
+  python $PROGRAM_PATH;
+  exit 0; # retorna 0 (sem erros)
+
+# caso contrário, printar erro e encerrar
+else
+  echo ;
+  echo -e "Program file cannot be found! $ERROR_ESCAPE < $PROGRAM_PATH > $RESET_ESCAPE";
+  echo ;
+  exit 1; # retorn 1 (erro)
+fi
