8f88579fd38c9de7979edaaa10f9842bb4927cf1
NASCC <pedro.main.tec@gmail.com>
2024-10-20 22:08:37 -0300

    [UPDATED] Docstring added

--- src/mod/pretty.py
+++ src/mod/pretty.py
@@ -1,10 +1,51 @@
 def clearTerminal() -> None:
+    """Limpa todo o conteúdo que foi printado no terminal.
+    Não retorna nada
+    """
 
     print('\033[H\033[2J', end = '')
 
 def getEscape(body_font       : int = 0,
               foreground_color: int = 0,
               background_color: int = 0) -> str:
+    r"""Retorna uma ANSI escape em forma de String recebendo apenas
+    três argumentos.
+
+    Parâmetros:
+        body_font: valor que determina a fonte, inicialmente
+                   como 0
+
+        foreground_color: valor que determina a cor da fonte,
+                          inicialmente como 0
+
+        background_color: valor que determina a cor do fundo da
+                          fonte, inicialmente como 0
+
+    Funcionamento: 
+        A função irá iniciar uma lista com apenas um elemento
+        (\033[0m). Após isso, os argumentos passados na função serão
+        colocados em variáveis de nome mais curto.
+
+        Serão iniciadas também outras quatro listas:
+            1. lista de valores esperados para body_font
+            2. lista de valores esperados para foreground_color
+            3. lista de valores esperados para background_color
+            4. list contendo um valor adicional para cada uma das
+               listas geradas acima
+
+        Agora, será feito um loop for a partir da função zip com
+        todos os elementos criados até agora.
+
+        Se o argumento inserido for diferente de 0 (fora do padrão),
+        então adicione uma string a lista final de resultados por
+        meio de operação ternária:
+            se o valor inserido estiver na lista de valores
+            esperados, insere, se não, coloque o valor substituto
+            determinado na lista 'case_not_in'.
+
+        Quando o loop encerrar, a função retorna uma string que junta
+        todos os elementos da string de resultados
+    """
 
     result: list[str] = ['\033[0m']
 
